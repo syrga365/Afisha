838e9073dc31894048a67309eea9085d8c692c4e
syrga365 <153297624+syrga365@users.noreply.github.com>
2024-03-19 14:20:05 +0600

    hw 8

--- Afisha/settings.py
+++ Afisha/settings.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
-PROXY_URL = config('PROXY_URL')
 
 ALLOWED_HOSTS = ['syrgataalaibekova.pythonanywhere.com']
 
