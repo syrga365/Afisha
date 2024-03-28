62290e635dfe66048f17b89153173bfa912d42c5
syrga365 <153297624+syrga365@users.noreply.github.com>
2024-03-28 16:11:57 +0600

    server

--- Afisha/settings.py
+++ Afisha/settings.py
@@ -68,16 +68,10 @@ WSGI_APPLICATION = 'Afisha.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'PORT': config('DB_PORT'),
-        'HOST': config('DB_HOST'),
-        'USER': config('DB_USER'),
-        "PASSWORD": config('DB_PASSWORD')
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
